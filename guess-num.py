bd106bbf9360d2b76dbc096fad6a27984ebb0497
Kevin Wang <kevinwbc1989@gmail.com>
2019-03-29 21:57:22 +0800

    Final version

--- guess-num.py
+++ guess-num.py
@@ -1,10 +1,14 @@
 import random
-r = random.randint(1, 100)
-count = 0
+start = input('請輸入起始值: ')
+end = input('請輸入結束值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
+count = 0 #猜的次數一開始為0
 while True:
 	num = input('請輸入一個數字: ')
 	num = int(num)
-	count = count + 1
+	count = count + 1 #每猜一次就加一
 	if num == r :
 		print('恭喜!你猜對了!')
 		print('這是你猜的第', count, '次')
